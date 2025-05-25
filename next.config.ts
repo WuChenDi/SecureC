3bb8c3ce10a3dafc5327ee7db5864707ca10a7c2
wudi <wuchendi96@gmail.com>
2025-05-25 10:11:39 +0800

    feat(next-config): Optimize image processing configuration
    - Enabled unoptimized image option to provide original quality images when needed
    - Added support for AVIF and WebP formats to improve image loading performance
    - Configured remote image mode to allow loading images from specified external sources

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   // output: 'export',
+  images: {
+    unoptimized: true,
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'notes-wudi.pages.dev'
+      }
+    ]
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.worker\.ts$/,
